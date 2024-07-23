0dffa9d93419af5faade047421d55c75021328ad
mouaammou <mouadamassine@gmail.com>
2024-07-23 13:29:23 +0100

    private routes, routes groups, the structure of nextjs project

---
+++ frontend/src/app/(auth)/register/page.jsx
@@ -0,0 +1,5 @@
+const Register = () => {
+	return <h1>register page</h1>;
+}
+ 
+export default Register;
---
+++ frontend/src/app/_lib/page.jsx
@@ -0,0 +1,7 @@
+const Private = () => {
+	return <>
+		<h1>this is a private folder, you cannot see it in the browser</h1>
+	</>;
+}
+ 
+export default Private;
